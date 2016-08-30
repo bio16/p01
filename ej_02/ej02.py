e3e2cd59153e3b9089833e7c8a4899f1d77b05df
spinto <spinto@df.uba.ar>
2016-08-30 19:50:02 -0300

    Histograma del 2 realizado

--- ej_02/ej02.py
+++ ej_02/ej02.py
@@ -4,6 +4,8 @@
 # Uso la librería igraph y pandas.
 import igraph
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
 
 # ------- Cargo la informacion del problema ------------- #
 
@@ -28,8 +30,72 @@ for i in range(len(dolphins_sex)):
                 vs["color"] = color_dict[vs['sex']]
             except:
                 vs["color"] = "green"
+                vs["sex"] = None
+
+# Cuento la cantidad de especies del mismo género
+genders = {}
+genders['f'] = 0
+genders['m'] = 0
+genders[None] = 0
+for vs in graph.vs:
+    genders[vs['sex']] += 1
+
+# Calculo la cantidad de links entre géneros que existen
+inter_gender_links = 0
+for vs in graph.vs:
+    neighbors = vs.neighbors()
+    for neighbour in neighbors:
+        if vs['sex'] != neighbour['sex'] and \
+           vs['sex'] != None and neighbour['sex'] != None: 
+            inter_gender_links += 1
+
+# Real gender links es los links entre géneros de la red real
+real_gender_links = inter_gender_links / 2
 
 
+
+# Paso a sortear los géneros entre los vértices manteniendo
+# la topología de la red inalterable. 
+# De cada sorteo calculo el número de links entre especies
+# de distinto género y devuelvo el histograma.
+
+list_of_genders = ['m'] * genders['m'] + ['f'] * genders['f'] + \
+                  [None] * genders[None]
+
+inter_gender_links_data = []
+
+for conf in range(10000):
+
+    random.shuffle(list_of_genders)
+    i = 0
+    for vs in graph.vs:
+        vs['sex'] = list_of_genders[i]
+        i += 1
+
+    inter_gender_links = 0
+    for vs in graph.vs:
+        neighbors = vs.neighbors()
+        for neighbour in neighbors:
+            if vs['sex'] != neighbour['sex'] and \
+              vs['sex'] != None and neighbour['sex'] != None: 
+                inter_gender_links += 1
+
+    inter_gender_links_data.append(inter_gender_links / 2)
+
+plt.hist(inter_gender_links_data, range = [40, 90], bins = 50, normed = True, label = u'Distribución nula')
+plt.plot([real_gender_links, real_gender_links], [0.0, 0.05], '.-', markersize = 15, label = 'Red real')
+plt.legend(loc = 2)
+plt.xlabel('# de Links')
+plt.savefig('Histrograma_b.eps')
+plt.show()
+
+
+
+
+
+
+
+"""
 # -------------- Distintos layouts ------------------- #
 
 # Grafo circular
@@ -69,3 +135,6 @@ layout = graph.layout_star()
 igraph.plot(graph, layout = layout, target = 'Star.eps')
 
 # --------------------------------------------------- #
+"""
+
+
